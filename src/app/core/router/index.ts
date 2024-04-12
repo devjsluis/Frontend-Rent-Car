d423226951a4c21dbff8a87f6abe8bccca41be81
reffcp <yo@cristiancovarrubias.online>
2024-04-11 19:51:20 -0600

    refactor: :recycle: reajuste de organizacion en carpetas desde la carpeta app

--- src/app/core/router/index.ts
+++ src/app/core/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AuthComponent from '../views/auth/AuthComponent.vue'
+import AuthComponent from '../../views/auth/AuthComponent.vue'
 
 const router = createRouter({
     history: createWebHistory('/'),
