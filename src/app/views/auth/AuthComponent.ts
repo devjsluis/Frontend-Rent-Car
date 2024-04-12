94adda5d53a4344965b98f4c031f1ea622ca80fd
devjsluis <devjsluis@gmail.com>
2024-04-12 11:44:41 -0600

    feat: :sparkles: Creación del Login
    Se agregó el formulario y su validación con Bootstrap

--- src/app/views/auth/AuthComponent.ts
+++ src/app/views/auth/AuthComponent.ts
@@ -1,7 +1,25 @@
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
 
 const AuthComponent = defineComponent({
-    name: "AuthComponent",
+  name: "AuthComponent",
+  setup() {
+    onMounted(() => {
+      const form = document.querySelector(
+        ".needs-validation"
+      ) as HTMLFormElement | null;
+
+      if (form) {
+        form.addEventListener("submit", (event) => {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+
+          form.classList.add("was-validated");
+        });
+      }
+    });
+  },
 });
 
 export default AuthComponent;
