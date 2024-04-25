555363be88611ee09eb5fede39a3cce8d96f8201
devjsluis <devjsluis@gmail.com>
2024-04-25 09:04:46 -0600

    feat: :sparkles: Spinner y Mensajes
    Se creó un componente de Spinner que será visible únicamente cuando se haga una petición y con Bootstrap manejamos los mensajes que se mostrarán en caso de éxito o error

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import SpinnerComponent from './app/components/SpinnerComponent.vue';
 </script>
 
 <template>
+  <SpinnerComponent />
   <RouterView />
 </template>
 
